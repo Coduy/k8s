326402d80de3a65e58567e17e43e310986455bc3
clavei1-admin <pokroy@clavei.es>
2024-09-23 08:37:05 +0200

    clean up the code

--- airflow_6/scripts/run.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-call kubectl delete deploy airflow-webserver
-call kubectl delete deploy airflow-scheduler
-call kubectl apply -f .\airflow-web.yaml
-call kubectl apply -f .\airflow-scheduler.yaml
