4fb0ee473cb896f01347dc4ccb6ebcb41385f62b
clavei1-admin <pokroy@clavei.es>
2024-09-23 08:29:04 +0200

    clean up the code

--- airflow_6/scale_up.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-call kubectl scale deployment airflow-webserver --replicas 1
-call kubectl scale deployment airflow-scheduler --replicas 1
-call kubectl scale deployment spark-master-deployment --replicas 1
-call kubectl scale deployment spark-worker-deployment --replicas 1
-call kubectl scale deployment hdfs-httpfs --replicas 1
