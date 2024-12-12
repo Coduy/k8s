59cd9e779b3340cbbb7dd6c5e09c30cdca2406f1
Pawel Okroy <pawel.okroy@teneo.ai>
2024-12-12 12:57:54 +0100

    housekeeping

--- python-dags/hdfs-test.py
+++
@@ -1,6 +0,0 @@
-from hdfs import InsecureClient
-
-client = InsecureClient('http://hdfs-release-datanode-0.hdfs-release-datanode.default.svc.cluster.local:50075', user='hdfs')
-
-# Example: Upload a file
-client.upload('/tmp/train01.csv', '/data/train01.csv')
--- scripts/delete_exec_pods.ps1
+++
@@ -1,6 +0,0 @@
-kubectl get pods --no-headers | ForEach-Object { 
-    if ($_.StartsWith("dag")) { 
-        $podName = $_.Split(" ")[0]
-        kubectl delete pod $podName 
-    } 
-}
